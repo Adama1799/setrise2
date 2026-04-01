3154e7e7a6ff7d5b4c79b423b7bfa5f1391b62b5
rkmbst <khaouni0rabah13@gmail.com>
2026-04-02 00:11:28 +0100

    Add app_colors.dart file with color definitions for SetRise app

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,8 @@
+// app_colors.dart
+
+class AppColors {
+  static const vibrantYellow = '#FFEB3B';
+  static const lightPink = '#FFC1D4';
+  static const lightGreen = '#B2FF59';
+  static const lightBlue = '#81D4FA';
+}
