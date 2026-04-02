53cb6badd49cdb77a58d565a13448f6256b3b10c
rkm2004europe <zexhup7@gmail.com>
2026-04-03 00:56:27 +0100

    Delete lib/domain/entities/user_entity.dart

--- lib/domain/entities/user_entity.dart
+++
@@ -1,8 +0,0 @@
-// user_entity.dart
-class UserEntity {
-  final String id;
-  final String username;
-  final String email;
-
-  UserEntity({required this.id, required this.username, required this.email});
-}
