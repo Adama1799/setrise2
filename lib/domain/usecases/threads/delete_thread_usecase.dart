7ecc43862c8239fc97447c05dde38041a0d97dba
rkmbst <khaouni0rabah13@gmail.com>
2026-04-05 01:42:29 +0100

    Update delete_thread_usecase.dart

--- lib/domain/usecases/threads/delete_thread_usecase.dart
+++ lib/domain/usecases/threads/delete_thread_usecase.dart
@@ -1,16 +1,14 @@
-// lib/domain/usecases/threads/delete_thread_usecase.dart
-// BUG FIX: This file was at the WRONG path:
-//   lib/domain/usecases/threads/lib/domain/usecases/threads/delete_thread_usecase.dart
-// service_locator.dart imports it from the correct path:
-//   '../../domain/usecases/threads/delete_thread_usecase.dart'
 import 'package:dartz/dartz.dart';
-import '../../errors/failures.dart';
+import '../../../core/errors/failures.dart';
+import '../../../core/usecases/usecase.dart';
 import '../../repositories/threads_repository.dart';
 
-class DeleteThreadUsecase {
+class DeleteThreadUseCase implements UseCase<void, String> {
   final ThreadsRepository _repository;
-  DeleteThreadUsecase(this._repository);
 
+  DeleteThreadUseCase(this._repository);
+
+  @override
   Future<Either<Failure, void>> call(String threadId) {
     return _repository.deleteThread(threadId);
   }
