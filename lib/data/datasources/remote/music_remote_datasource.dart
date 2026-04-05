6df4877b4b8d7190d22a9eb817fb2919b19c2f5a
rkmbst <khaouni0rabah13@gmail.com>
2026-04-05 01:39:50 +0100

    Update music_remote_datasource.dart

--- lib/data/datasources/remote/music_remote_datasource.dart
+++ lib/data/datasources/remote/music_remote_datasource.dart
@@ -1,124 +1,122 @@
-// lib/data/datasources/remote/music_remote_datasource.dart
-import '../../../core/network/api_client.dart';
+import 'package:dartz/dartz.dart';
+import '../../../core/constants/api_endpoints.dart';  // ⬅️ أضف هذا
 import '../../../core/errors/exceptions.dart';
-import '../../../core/constants/app_constants.dart';
-import '../../models/music_track_model.dart';
+import '../../../core/network/dio_client.dart';
+import '../../../domain/entities/music/track_entity.dart';
+import '../../../domain/entities/music/playlist_entity.dart';
+import '../../models/music/track_model.dart';
+import '../../models/music/playlist_model.dart';
 
 abstract class MusicRemoteDataSource {
-  Future<List<MusicTrackModel>> getTracks(int page);
-  Future<List<MusicTrackModel>> getTrendingTracks();
-  Future<MusicTrackModel> getTrack(String trackId);
-  Future<List<MusicTrackModel>> searchTracks(String query);
-  Future<List<MusicTrackModel>> getPlaylist(String playlistId);
-  Future<MusicTrackModel> likeTrack(String trackId);
-  Future<MusicTrackModel> unlikeTrack(String trackId);
-  Future<List<MusicTrackModel>> getFavoriteTracks();
+  Future<List<TrackEntity>> getTrendingTracks();
+  Future<List<TrackEntity>> searchTracks(String query);
+  Future<TrackEntity> getTrack(String trackId);
+  Future<List<PlaylistEntity>> getUserPlaylists();
+  Future<PlaylistEntity> getPlaylist(String playlistId);
+  Future<void> likeTrack(String trackId);
+  Future<void> unlikeTrack(String trackId);
+  Future<List<TrackEntity>> getFavoriteTracks();
 }
 
 class MusicRemoteDataSourceImpl implements MusicRemoteDataSource {
-  final ApiClient apiClient;
+  final DioClient apiClient;
 
   MusicRemoteDataSourceImpl(this.apiClient);
 
   @override
-  Future<List<MusicTrackModel>> getTracks(int page) async {
-    try {
-      final response = await apiClient.get(
-        '${ApiEndpoints.musicEndpoint}?page=$page&limit=${AppConstants.pageSize}',
-      );
-      return (response as List)
-          .map((e) => MusicTrackModel.fromJson(e))
-          .toList();
-    } catch (e) {
-      throw ServerException(e.toString());
-    }
-  }
-
-  @override
-  Future<List<MusicTrackModel>> getTrendingTracks() async {
+  Future<List<TrackEntity>> getTrendingTracks() async {
     try {
       final response = await apiClient.get('${ApiEndpoints.musicEndpoint}/trending');
-      return (response as List)
-          .map((e) => MusicTrackModel.fromJson(e))
+      return (response.data as List)
+          .map((track) => TrackModel.fromJson(track))
           .toList();
     } catch (e) {
-      throw ServerException(e.toString());
+      throw ServerException(message: e.toString());
     }
   }
 
   @override
-  Future<MusicTrackModel> getTrack(String trackId) async {
-    try {
-      final response = await apiClient.get('${ApiEndpoints.musicEndpoint}/$trackId');
-      return MusicTrackModel.fromJson(response);
-    } catch (e) {
-      throw ServerException(e.toString());
-    }
-  }
-
-  @override
-  Future<List<MusicTrackModel>> searchTracks(String query) async {
+  Future<List<TrackEntity>> searchTracks(String query) async {
     try {
       final response = await apiClient.get(
-        '${ApiEndpoints.musicEndpoint}/search?q=$query',
+        '${ApiEndpoints.musicEndpoint}/search',
+        queryParameters: {'q': query},
       );
-      return (response as List)
-          .map((e) => MusicTrackModel.fromJson(e))
+      return (response.data as List)
+          .map((track) => TrackModel.fromJson(track))
           .toList();
     } catch (e) {
-      throw ServerException(e.toString());
+      throw ServerException(message: e.toString());
     }
   }
 
   @override
-  Future<List<MusicTrackModel>> getPlaylist(String playlistId) async {
+  Future<TrackEntity> getTrack(String trackId) async {
+    try {
+      final response = await apiClient.get('${ApiEndpoints.musicEndpoint}/$trackId');
+      return TrackModel.fromJson(response.data);
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Future<List<PlaylistEntity>> getUserPlaylists() async {
+    try {
+      final response = await apiClient.get('${ApiEndpoints.musicEndpoint}/playlists');
+      return (response.data as List)
+          .map((playlist) => PlaylistModel.fromJson(playlist))
+          .toList();
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Future<PlaylistEntity> getPlaylist(String playlistId) async {
     try {
       final response = await apiClient.get(
         '${ApiEndpoints.musicEndpoint}/playlist/$playlistId',
       );
-      return (response as List)
-          .map((e) => MusicTrackModel.fromJson(e))
-          .toList();
+      return PlaylistModel.fromJson(response.data);
     } catch (e) {
-      throw ServerException(e.toString());
+      throw ServerException(message: e.toString());
     }
   }
 
   @override
-  Future<MusicTrackModel> likeTrack(String trackId) async {
+  Future<void> likeTrack(String trackId) async {
     try {
-      final response = await apiClient.post(
+      await apiClient.post(
         '${ApiEndpoints.musicEndpoint}/$trackId/like',
-        {},
+        data: {},
       );
-      return MusicTrackModel.fromJson(response);
     } catch (e) {
-      throw ServerException(e.toString());
+      throw ServerException(message: e.toString());
     }
   }
 
   @override
-  Future<MusicTrackModel> unlikeTrack(String trackId) async {
+  Future<void> unlikeTrack(String trackId) async {
     try {
-      final response = await apiClient.post(
+      await apiClient.post(
         '${ApiEndpoints.musicEndpoint}/$trackId/unlike',
-        {},
+        data: {},
       );
-      return MusicTrackModel.fromJson(response);
     } catch (e) {
-      throw ServerException(e.toString());
+      throw ServerException(message: e.toString());
     }
   }
 
   @override
-  Future<List<MusicTrackModel>> getFavoriteTracks() async {
+  Future<List<TrackEntity>> getFavoriteTracks() async {
     try {
       final response = await apiClient.get('${ApiEndpoints.musicEndpoint}/favorites');
-      return (response as List)
-          .map((e) => MusicTrackModel.fromJson(e))
+      return (response.data as List)
+          .map((track) => TrackModel.fromJson(track))
           .toList();
     } catch (e) {
-      throw ServerException(e.toString());
+      throw ServerException(message: e.toString());
     }
   }
 }
