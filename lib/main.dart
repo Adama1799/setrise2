b4fcba3bccac547f456397679f51adf398d58265
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-04-11 02:51:51 +0100

    Update main screen to use MainNavigationScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'core/theme/app_colors.dart';
+
 import 'core/theme/app_theme.dart';
-import 'presentation/screens/main/main_screen.dart';
+import 'presentation/screens/main_navigation_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,19 +10,22 @@ void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Brightness.light,
+    systemNavigationBarColor: Colors.black,
+    systemNavigationBarIconBrightness: Brightness.light,
   ));
   runApp(const SetRiseApp());
 }
 
 class SetRiseApp extends StatelessWidget {
   const SetRiseApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SetRise',
       theme: AppTheme.darkTheme,
-      home: const MainScreen(),
+      home: const MainNavigationScreen(),
     );
   }
 }
