3ae4ce1668d775e7887dbf94ba0b6a89424db9dd
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-04-12 17:31:37 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'core/theme/app_colors.dart';
 import 'core/theme/app_theme.dart';
-import 'presentation/screens/main_navigation_screen.dart';
+import 'presentation/screens/main/main_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,24 +10,19 @@ void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Brightness.light,
-    systemNavigationBarColor: Color(0xFF010101),
-    systemNavigationBarIconBrightness: Brightness.light,
   ));
   runApp(const SetRiseApp());
 }
 
 class SetRiseApp extends StatelessWidget {
   const SetRiseApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SetRise',
-      themeMode: ThemeMode.dark,
       theme: AppTheme.darkTheme,
-      darkTheme: AppTheme.darkTheme,
-      home: const MainNavigationScreen(),
+      home: const MainScreen(),
     );
   }
 }
