3ad7c5fc42cf71efb7ccb01d0bba984dc8d0c3d8
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-04-16 01:37:57 +0100

    Add CategoryModel class for category data structure

---
+++ lib/data/models/category_model.dart
@@ -0,0 +1,9 @@
+// lib/data/models/category_model.dart
+
+class CategoryModel {
+  final String id;
+  final String name;
+  final String iconUrl;
+
+  CategoryModel({required this.id, required this.name, required this.iconUrl});
+}
