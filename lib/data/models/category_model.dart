1b5bffbd57204160aa26e32935898a821ea26b68
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-04-18 00:46:35 +0100

    Refactor CategoryModel to include color and icon

--- lib/data/models/category_model.dart
+++ lib/data/models/category_model.dart
@@ -1,9 +1,17 @@
 // lib/data/models/category_model.dart
 
+import 'package:flutter/material.dart';
+
 class CategoryModel {
   final String id;
   final String name;
-  final String iconUrl;
+  final String icon;
+  final Color color;
 
-  CategoryModel({required this.id, required this.name, required this.iconUrl});
+  CategoryModel({
+    required this.id,
+    required this.name,
+    required this.icon,
+    required this.color,
+  });
 }
