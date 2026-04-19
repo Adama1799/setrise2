ee3cd6a91e2087a552242637e2cf65e2a0a05cf2
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-04-19 01:12:49 +0100

    Refactor SellerModel to include storeName and avatarUrl

--- lib/data/models/seller_model.dart
+++ lib/data/models/seller_model.dart
@@ -1,15 +1,16 @@
 // lib/data/models/seller_model.dart
-
 class SellerModel {
   final String id;
-  final String name;
-  final String avatar;
+  final String storeName;
+  final String avatarUrl;
   final bool isVerified;
+  final double rating;
 
   SellerModel({
     required this.id,
-    required this.name,
-    required this.avatar,
+    required this.storeName,
+    required this.avatarUrl,
     this.isVerified = false,
+    this.rating = 4.5,
   });
 }
