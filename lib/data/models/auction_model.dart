63e97894e734123526f81660af8409fff8caf333
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-04-19 01:13:21 +0100

    Refactor AuctionModel properties and add formattedTimeLeft
    Renamed 'title' to 'name' and added 'bidCount' with default value.

--- lib/data/models/auction_model.dart
+++ lib/data/models/auction_model.dart
@@ -1,17 +1,24 @@
 // lib/data/models/auction_model.dart
-
 class AuctionModel {
   final String id;
-  final String title;
+  final String name;
   final double currentBid;
   final String imageUrl;
   final DateTime endTime;
+  final int bidCount;
 
   AuctionModel({
     required this.id,
-    required this.title,
+    required this.name,
     required this.currentBid,
     required this.imageUrl,
     required this.endTime,
+    this.bidCount = 0,
   });
+
+  String get formattedTimeLeft {
+    final diff = endTime.difference(DateTime.now());
+    if (diff.isNegative) return 'Ended';
+    return '${diff.inHours.toString().padLeft(2, '0')}:${diff.inMinutes.remainder(60).toString().padLeft(2, '0')}:${diff.inSeconds.remainder(60).toString().padLeft(2, '0')}';
+  }
 }
