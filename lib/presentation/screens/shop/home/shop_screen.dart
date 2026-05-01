d8b0d4d52a16780fcd1170176fcceded5b5cd8b7
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-01 18:14:39 +0100

    Add ShopScreen widget for shop home

---
+++ lib/presentation/screens/shop/home/shop_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'shop_home_screen.dart';
+
+class ShopScreen extends StatelessWidget {
+  const ShopScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const ShopHomeScreen();
+}
