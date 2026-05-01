3c02e985657473a01e283da8a8a47e83b52936ae
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-01 17:33:30 +0100

    Add checkout models for payment and address
    Define enums and classes for checkout process.

---
+++ lib/presentation/screens/shop/checkout_models.dart
@@ -0,0 +1,16 @@
+// lib/presentation/screens/shop/checkout_models.dart
+
+enum PaymentType { creditCard, paypal, cashOnDelivery }
+
+class Address {
+  final String id, name, fullName, street, city, state, zipCode, country, phone;
+  final bool isDefault;
+  Address({required this.id, required this.name, required this.fullName, required this.street, required this.city, required this.state, required this.zipCode, required this.country, required this.phone, this.isDefault = false});
+}
+
+class PaymentMethod {
+  final String id, name, details;
+  final PaymentType type;
+  final IconData icon;
+  PaymentMethod({required this.id, required this.type, required this.name, required this.icon, required this.details});
+}
