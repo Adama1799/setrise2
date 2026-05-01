431a00ffd4755950075519fda7ff1a71f19d22fa
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-01 18:07:44 +0100

    Add StoresSection widget to display top stores

---
+++ lib/presentation/screens/shop/home/widgeta/stores_section.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import '../../../../../core/theme/app_colors.dart';
+import '../../../../data/models/store_model.dart';
+import '../../widgets/store_card.dart';
+
+class StoresSection extends StatelessWidget {
+  const StoresSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final stores = Store.getMockStores().take(3).toList();
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          const Text('Top Stores', style: TextStyle(color: AppColors.white, fontSize: 18, fontWeight: FontWeight.bold)),
+          CupertinoButton(padding: EdgeInsets.zero, child: const Text('View All', style: TextStyle(color: AppColors.shop)), onPressed: () {}),
+        ]),
+      ),
+      SizedBox(
+        height: 200,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          itemCount: stores.length,
+          itemBuilder: (_, i) => SizedBox(width: 280, child: Padding(padding: const EdgeInsets.all(8), child: StoreCard(store: stores[i]))),
+        ),
+      ),
+    ]);
+  }
+}
