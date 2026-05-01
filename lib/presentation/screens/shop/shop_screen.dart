211fc8232335539c7cd03e6767517309908eac2f
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-01 18:26:02 +0100

    Add ShopScreen with ShopMainTabs widget

---
+++ lib/presentation/screens/shop/shop_screen.dart
@@ -0,0 +1,7 @@
+// lib/presentation/screens/shop/shop_screen.dart
+import 'shop_main_tabs.dart';
+class ShopScreen extends StatelessWidget {
+  const ShopScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const ShopMainTabs();
+}
