f89cfe9912d63a5bb42d734ff93bf6600dfb72ba
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-02 18:37:34 +0100

    Refactor Address and PaymentMethod constructors

--- lib/presentation/screens/shop/checkout_models.dart
+++ lib/presentation/screens/shop/checkout_models.dart
@@ -1,16 +1,36 @@
 // lib/presentation/screens/shop/checkout_models.dart
+import 'package:flutter/cupertino.dart';
 
 enum PaymentType { creditCard, paypal, cashOnDelivery }
 
 class Address {
   final String id, name, fullName, street, city, state, zipCode, country, phone;
   final bool isDefault;
-  Address({required this.id, required this.name, required this.fullName, required this.street, required this.city, required this.state, required this.zipCode, required this.country, required this.phone, this.isDefault = false});
+
+  Address({
+    required this.id,
+    required this.name,
+    required this.fullName,
+    required this.street,
+    required this.city,
+    required this.state,
+    required this.zipCode,
+    required this.country,
+    required this.phone,
+    this.isDefault = false,
+  });
 }
 
 class PaymentMethod {
   final String id, name, details;
   final PaymentType type;
   final IconData icon;
-  PaymentMethod({required this.id, required this.type, required this.name, required this.icon, required this.details});
+
+  PaymentMethod({
+    required this.id,
+    required this.type,
+    required this.name,
+    required this.icon,
+    required this.details,
+  });
 }
