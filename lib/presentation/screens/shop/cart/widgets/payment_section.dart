cfe14d90fbe9c0a9276b9b2d9d2c0628be6642db
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-02 18:32:26 +0100

    Refactor PaymentSection widget for improved readability

--- lib/presentation/screens/shop/cart/widgets/payment_section.dart
+++ lib/presentation/screens/shop/cart/widgets/payment_section.dart
@@ -1,5 +1,7 @@
 // lib/presentation/screens/shop/cart/widgets/payment_section.dart
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:setrise/core/theme/app_colors.dart';
 import '../../checkout_models.dart';
 
 class PaymentSection extends StatelessWidget {
@@ -7,18 +9,24 @@ class PaymentSection extends StatelessWidget {
   final int selectedIndex;
   final ValueChanged<int> onChanged;
 
-  const PaymentSection({super.key, required this.methods, required this.selectedIndex, required this.onChanged});
+  const PaymentSection({
+    super.key,
+    required this.methods,
+    required this.selectedIndex,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: methods.asMap().entries.map((e) {
+      children: methods.asMap().entries.map<Widget>((e) {
         final i = e.key;
         final m = e.value;
         return RadioListTile<int>(
           value: i,
           groupValue: selectedIndex,
           onChanged: (v) => onChanged(v!),
+          activeColor: AppColors.shop,
           title: Text(m.name, style: const TextStyle(color: CupertinoColors.white)),
           subtitle: Text(m.details, style: const TextStyle(color: CupertinoColors.systemGrey)),
         );
