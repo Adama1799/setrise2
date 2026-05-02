821e066d39c27283941a9f034616e788ef613fbb
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-02 01:03:16 +0100

    Refactor MockShopService methods to single-line syntax

--- lib/data/services/mock_shop_service.dart
+++ lib/data/services/mock_shop_service.dart
@@ -2,12 +2,6 @@
 import '../models/product_model.dart';
 
 class MockShopService {
-  static Future<List<ProductModel>> getSimilarProducts(ProductModel product) async {
-    // إرجاع قائمة فارغة مؤقتًا
-    return [];
-  }
-
-  static Future<List<ProductModel>> getAllProducts() async {
-    return Future.delayed(const Duration(milliseconds: 500), () => []);
-  }
+  static Future<List<ProductModel>> getSimilarProducts(ProductModel p) async => [];
+  static Future<List<ProductModel>> getAllProducts() async => [];
 }
