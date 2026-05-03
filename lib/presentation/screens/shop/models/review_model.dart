62b141d96136316e169481697cb9d6e14cd42baf
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-03 12:08:12 +0100

    Add ReviewModel class for product reviews

---
+++ lib/presentation/screens/shop/models/review_model.dart
@@ -0,0 +1,17 @@
+// lib/presentation/screens/shop/models/review_model.dart
+class ReviewModel {
+  final String id, productId, userName, comment;
+  final double rating;
+  final String? imageUrl;
+  final DateTime date;
+
+  ReviewModel({
+    required this.id,
+    required this.productId,
+    required this.userName,
+    required this.rating,
+    required this.comment,
+    this.imageUrl,
+    DateTime? date,
+  }) : date = date ?? DateTime.now();
+}
