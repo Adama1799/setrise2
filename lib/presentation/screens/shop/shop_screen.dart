1ea3d77aa51fcf2abb335c1c76e11ed7f5a64dcb
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-03 23:49:25 +0100

    Refactor ShopScreen to use HomeScreen from material package

--- lib/presentation/screens/shop/shop_screen.dart
+++ lib/presentation/screens/shop/shop_screen.dart
@@ -1,10 +1,9 @@
 // lib/presentation/screens/shop/shop_screen.dart
-import 'package:flutter/cupertino.dart';
-import 'shop_home_screen.dart';
+import 'package:flutter/material.dart';
+import 'material_shop/screens/home/home_screen.dart';
 
 class ShopScreen extends StatelessWidget {
   const ShopScreen({super.key});
-
   @override
-  Widget build(BuildContext context) => const ShopHomeScreen();
+  Widget build(BuildContext context) => const HomeScreen();
 }
