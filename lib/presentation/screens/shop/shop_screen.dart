be05e13740b5919f78a4b41ada11e036f5b6160c
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-04 00:54:35 +0100

    Refactor ShopScreen to use StatefulWidget with navigation

--- lib/presentation/screens/shop/shop_screen.dart
+++ lib/presentation/screens/shop/shop_screen.dart
@@ -1,9 +1,41 @@
 // shop_screen.dart
 import 'package:flutter/material.dart';
 import 'material_shop/screens/home/home_screen.dart';
+import 'material_shop/screens/search/search_screen.dart';
+import 'material_shop/screens/cart/cart_screen.dart';
+import 'material_shop/screens/account/profile_screen.dart';
 
-class ShopScreen extends StatelessWidget {
+class ShopScreen extends StatefulWidget {
   const ShopScreen({super.key});
+
   @override
-  Widget build(BuildContext context) => const HomeScreen();
+  State<ShopScreen> createState() => _ShopScreenState();
+}
+
+class _ShopScreenState extends State<ShopScreen> {
+  int _currentIndex = 0;
+  final _screens = const [
+    HomeScreen(),
+    SearchScreen(),
+    CartScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedItemColor: const Color(0xFFFF7643),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
 }
