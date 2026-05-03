9f60745c03df69890ea8a76f6714b466afa87825
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-04 00:01:46 +0100

    Add constants for shop UI styling
    Define constants for colors, durations, and text styles.

---
+++ lib/presentation/screens/shop/material_shop/constants.dart
@@ -0,0 +1,15 @@
+// material_shop/constants.dart
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFFFF7643);
+const Color kPrimaryLightColor = Color(0xFFFFECDF);
+const Color kSecondaryColor = Color(0xFF979797);
+const Color kTextColor = Color(0xFF000000);
+const Duration kAnimationDuration = Duration(milliseconds: 200);
+const TextStyle headingStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  color: kTextColor,
+  height: 1.5,
+);
+const Duration defaultDuration = Duration(milliseconds: 250);
