8e5ffe360d2eea6089b1a0f1eb40ad784e24e31c
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-06 00:20:12 +0100

    Add Debouncer class for debouncing actions

---
+++ lib/presentation/screens/shop/material_shop/utils/debouncer.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+class Debouncer {
+  final Duration duration;
+  Timer? _timer;
+
+  Debouncer({required this.duration});
+
+  void run(VoidCallback action) {
+    _timer?.cancel();
+    _timer = Timer(duration, action);
+  }
+
+  void dispose() => _timer?.cancel();
+}
