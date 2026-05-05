9228247bf7e36e3b2150f5f653aff8cf0f6f7a29
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-06 00:13:01 +0100

    Add AppDimensions class for layout constants

---
+++ lib/presentation/screens/shop/material_shop/theme/app_dimensions.dart
@@ -0,0 +1,27 @@
+class AppDimensions {
+  AppDimensions._();
+
+  static const double xxs = 2.0;
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 12.0;
+  static const double lg = 16.0;
+  static const double xl = 24.0;
+  static const double xxl = 32.0;
+  static const double sectionGap = 32.0;
+  static const double gridGap = 12.0;
+  static const double cardInner = 10.0;
+  static const double radiusXs = 4.0;
+  static const double radiusSm = 8.0;
+  static const double radiusMd = 12.0;
+  static const double radiusLg = 16.0;
+  static const double radiusXxl = 24.0;
+  static const double radiusFull = 999.0;
+  static const double ratingStarSm = 14.0;
+  static const double ratingStarMd = 20.0;
+  static const double buttonHeightLg = 48.0;
+  static const double buttonHeightSm = 36.0;
+  static const double inputHeight = 48.0;
+  static const double minTapTarget = 44.0;
+  static const double avatarLg = 80.0;
+}
