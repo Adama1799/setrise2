ce462a6a606b98ad671afe433079eab67ab5cf5d
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-06 00:18:57 +0100

    Add Haptics utility class for feedback

---
+++ lib/presentation/screens/shop/material_shop/utils/haptics.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/services.dart';
+
+class Haptics {
+  static void light() => HapticFeedback.lightImpact();
+  static void medium() => HapticFeedback.mediumImpact();
+  static void heavy() => HapticFeedback.heavyImpact();
+}
