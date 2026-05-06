0a6876c0b5d1a62daf1e6b579d3908f9aef3eb0a
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-06 21:06:41 +0100

    Refactor ShopScreen build method for brevity

--- lib/presentation/screens/shop/shop_screen.dart
+++ lib/presentation/screens/shop/shop_screen.dart
@@ -1,11 +1,9 @@
+// lib/presentation/screens/shop/shop_screen.dart
 import 'package:flutter/material.dart';
 import 'material_shop/screens/home/home_screen.dart';
 
 class ShopScreen extends StatelessWidget {
   const ShopScreen({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    return const HomeScreen();
-  }
+  Widget build(BuildContext context) => const HomeScreen();
 }
