2376ea2930f2686d085c32d0b5a39870efeffa9f
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-06 02:06:41 +0100

    Refactor ChatService to CartService with item management

--- lib/presentation/screens/shop/material_shop/services/chat_service.dart
+++ lib/presentation/screens/shop/material_shop/services/chat_service.dart
@@ -1,14 +1,39 @@
-// services/chat_service.dart
-import '../models/chat_model.dart';
+import 'package:flutter/material.dart';
+import '../models/cart_model.dart';
+import '../models/product_model.dart';
 
-class ChatService {
-  final List<ChatMessage> _messages = [];
-  List<ChatMessage> getMessages(String storeId) {
-    // محاكاة رسائل قديمة
-    return _messages.where((m) => m.storeId == storeId).toList();
+class CartService extends ChangeNotifier {
+  final List<CartItem> _items = [];
+  List<CartItem> get items => List.unmodifiable(_items);
+  int get itemCount => _items.fold(0, (sum, item) => sum + item.quantity);
+  double get totalPrice => _items.fold(0, (sum, item) => sum + item.product.price * item.quantity);
+
+  void addItem(ProductModel product, {int quantity = 1}) {
+    final index = _items.indexWhere((i) => i.product.id == product.id);
+    if (index >= 0) {
+      _items[index].quantity += quantity;
+    } else {
+      _items.add(CartItem(product: product, quantity: quantity));
+    }
+    notifyListeners();
   }
 
-  void sendMessage(ChatMessage message) {
-    _messages.add(message);
+  void removeItem(String productId) {
+    _items.removeWhere((i) => i.product.id == productId);
+    notifyListeners();
+  }
+
+  void increment(int index) {
+    _items[index].quantity++;
+    notifyListeners();
+  }
+
+  void decrement(int index) {
+    if (_items[index].quantity > 1) {
+      _items[index].quantity--;
+    } else {
+      _items.removeAt(index);
+    }
+    notifyListeners();
   }
 }
