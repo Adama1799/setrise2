9701b70525cd0af0fff2b66d1df2a0e593daf701
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-05-08 00:12:36 +0100

    Integrate GoRouter for navigation in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'core/theme/app_colors.dart';
+import 'package:go_router/go_router.dart';
 import 'core/theme/app_theme.dart';
 import 'presentation/screens/main/main_screen.dart';
+import 'presentation/screens/shop/material_shop/models/product_model.dart';
+import 'presentation/screens/shop/material_shop/screens/product/product_page.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const MainScreen(),
+    ),
+    GoRoute(
+      path: '/product/:id',
+      builder: (context, state) {
+        final product = state.extra as ProductModel;
+        return ProductPage(product: product);
+      },
+    ),
+  ],
+);
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,13 +39,14 @@ void main() {
 
 class SetRiseApp extends StatelessWidget {
   const SetRiseApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'SetRise',
       theme: AppTheme.darkTheme,
-      home: const MainScreen(),
+      routerConfig: _router,
     );
   }
 }
